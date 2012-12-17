dd762aeaeeca6105bd2acc4d9e93622bd5ebac00
Cole Diamond <cd1am0nd@yahoo.com>
2012-12-17 05:30:32 -0500

    asdf

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ActiveRecord::Base
   attr_accessible :likee_id, :liker_id
   belongs_to :user
-  belongs_to :liker, :class_name => 'User', :foreign_key => "likee_id"
+  belongs_to :liker, :class_name => 'User', :foreign_key => "liker_id"
   belongs_to :liked, :class_name => "User", :foreign_key => "likee_id"
 
 end
